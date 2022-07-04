5ed79110a07b155991c88d4e1619fd57e83b2a62
Ultrasour8 <tristan.mattei@gmail.com>
2022-07-04 15:27:25 +0200

    PROJET FINAL

--- 31-PDO/update.php
+++ 31-PDO/update.php
@@ -1,9 +1,25 @@
 <?php 
 session_start();
+require_once("PDO.php");
 
 echo '<pre>';
 print_r($_SESSION);
 echo '</pre>';
+$name =$_SESSION['user']['name'];
+$_SESSION['user']['password'];
+
+if(isset($_POST["update"])){
+$sql = "UPDATE users(name, email,password) SET (:name, :email, :password) WHERE id= 'id'";
+echo ("<pre>" . $sql . "</pre>");
+$stat = $pdo->prepare($sql);
+$stat->execute([
+    ":name"=>$_SESSION["name"],
+    ":email"=>$_SESSION["email"],
+    ":password"=>$_SESSION["password"],
+    ":id" => $_SESSION["id"],
+]);
+header("Location: usr.php");
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,34 +32,34 @@ echo '</pre>';
 </head>
 <body>
     <?php
-echo "<table border='1'>";
-foreach($rows as $row){
-$tab = <<<TACOS
-    <tr>
-        <td>{$row['name']}</td>
-        <td>{$row['email']}</td>
-        <td>{$row['password']}</td>
-        <td>
-        <form method="POST">
-            <input type="hidden" name="user_id" value="{$row["user_id"]}">
-            <input type="submit" name="delete" value="supprimer">
-            </form>
-        </td>
-        <td>
-            <input type="submit" name="update" value="update">
-        </td>
-    </tr>
-TACOS;
-echo $tab;
-}
-echo "</table>";
+// echo "<table border='1'>";
+// foreach($rows as $row){
+// $tab = <<<TACOS
+//     <tr>
+//         <td>{$_SESSION['name']}</td>
+//         <td>{$_SESSION["email"]}</td>
+//         <td>{$_SESSION["password"]}</td>
+//         <td>
+//         <form method="POST">
+//             <input type="hidden" name="user_id" value="{$row["user_id"]}">
+//             <input type="submit" name="delete" value="supprimer">
+//             </form>
+//         </td>
+//         <td>
+//             <input type="submit" name="update" value="update">
+//         </td>
+//     </tr>
+// TACOS;
+// echo $tab;
+// }
+// echo "</table>";
 ?>
-    <p>ajouter un utilisateur</p>
+    <p>Modifier les données</p>
     <form action="" method="POST">
-    <p>nom: <input type="text" name="name"></p>
-    <p>email: <input type="email" name="email"></p>
-    <p>password: <input type="password" name="password"></p>
-    <input type="submit" value="Ajouter">
+    <p>nom: <input type="text" name="name" placeholder="<?php echo $name  ?>"></p>
+    <p>email: <input type="email" name="email" placeholder="<?php echo $_SESSION['user']['email'] ?>" ></p>
+    <p>password: <input type="password" name="password" placeholder="<?php echo $_SESSION['user']['password'] ?>"></p>
+    <input type="submit" value="Mettre à Jour">
 </form>
 </body>
 </html>
